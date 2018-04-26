95d7929db8ccfee7fbb3ec299d51ee21a969eb94
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-04-26 16:39:55 -0300

    Melhorias no CMakeLists.txt e no layout do código.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
-class MyApp: public wxApp
-{
+
+class MyApp: public wxApp {
 public:
     virtual bool OnInit();
 };
-class MyFrame: public wxFrame
-{
+
+class MyFrame: public wxFrame {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 private:
@@ -17,25 +17,26 @@ private:
     void OnAbout(wxCommandEvent& event);
     wxDECLARE_EVENT_TABLE();
 };
-enum
-{
+
+enum {
     ID_Hello = 1
 };
+
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(ID_Hello,   MyFrame::OnHello)
     EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
     EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 wxEND_EVENT_TABLE()
 wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit()
-{
+
+bool MyApp::OnInit() {
     MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
     frame->Show( true );
     return true;
 }
+
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
-{
+        : wxFrame(NULL, wxID_ANY, title, pos, size) {
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
                      "Help string shown in status bar for this menu item");
@@ -50,16 +51,16 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     CreateStatusBar();
     SetStatusText( "Welcome to wxWidgets!" );
 }
-void MyFrame::OnExit(wxCommandEvent& event)
-{
+
+void MyFrame::OnExit(wxCommandEvent& event) {
     Close( true );
 }
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
+
+void MyFrame::OnAbout(wxCommandEvent& event) {
     wxMessageBox( "This is a wxWidgets' Hello world sample",
                   "About Hello World", wxOK | wxICON_INFORMATION );
 }
-void MyFrame::OnHello(wxCommandEvent& event)
-{
+
+void MyFrame::OnHello(wxCommandEvent& event) {
     wxLogMessage("Hello world from wxWidgets!");
 }
