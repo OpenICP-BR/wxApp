f0992e82246e08e14dea065817713979dccfc89a
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-04-26 21:58:45 -0300

    Usando XRC

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "main.h"
-#include "mainframe.h"
 
 wxIMPLEMENT_APP(ICPApp);
 
 bool ICPApp::OnInit() {
-    MainFrame *frame = new MainFrame(this->GetTopWindow());
-    frame->Show( true );
+    wxXmlResource::Get()->InitAllHandlers();
+    wxXmlResource::Get()->Load("src/ui.xrc");
+
+    wxFrame *frame = wxXmlResource::Get()->LoadFrame(this->GetTopWindow(), "MainFrame");
+    if (frame != NULL) {
+        frame->Show(true);
+    }
     return true;
 }
--- src/main.h
+++ src/main.h
@@ -3,6 +3,8 @@
     #include <wx/wx.h>
 #endif
 
+#include "wx/xrc/xmlres.h"
+
 class ICPApp: public wxApp {
 public:
     virtual bool OnInit();
