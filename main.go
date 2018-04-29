0eaba36d09c6cdfb238c0b5b099d893f8ace39ec
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-04-29 20:41:51 -0300

    ?

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/dontpanic92/wxGo/wx"
+	"fmt"
 )
 
 func main() {
 	wx.NewApp()
+	fmt.Printf("%+v\n", wx.NewXmlResource().LoadAllFiles("ui.xrc"))
 	f := wx.NewDialog(wx.NullWindow, -1, "Hello World")
 	f.ShowModal()
 	f.Destroy()
