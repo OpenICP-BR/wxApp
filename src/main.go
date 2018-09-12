788645ff61737889c2881ed5a28cdc14d7eceab5
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-09-11 21:26:56 -0300

    Added comments.

--- src/main.go
+++ src/main.go
@@ -5,18 +5,18 @@ package main
 // #cgo LDFLAGS: -lstdc++ -L/usr/local/lib   -framework IOKit -framework Carbon -framework Cocoa -framework AudioToolbox -framework System -framework OpenGL -lwx_osx_cocoau_xrc-3.0 -lwx_osx_cocoau_webview-3.0 -lwx_osx_cocoau_html-3.0 -lwx_osx_cocoau_qa-3.0 -lwx_osx_cocoau_adv-3.0 -lwx_osx_cocoau_core-3.0 -lwx_baseu_xml-3.0 -lwx_baseu_net-3.0 -lwx_baseu-3.0
 import "C"
 import "os"
-import "fmt"
 
 func main() {
-	fmt.Println(os.Args)
-
+	// Convert CLI arguments to array of C strings
 	args := C.str_array_new(C.int(len(os.Args)))
 	for i, v := range os.Args {
 		str := C.CString(v)
 		C.str_array_set(args, C.int(i), str)
 	}
 
+	// Start the app
 	C.start_app(C.int(1), args)
 
+	// Clear some memory
 	C.str_array_free(C.int(len(os.Args)), args)
 }
